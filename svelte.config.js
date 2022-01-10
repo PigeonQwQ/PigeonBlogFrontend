016370b9afea64bf99f7ae82b7e7dd03b1bd43b8
DarcJC <me@darc.pro>
2022-01-10 20:42:03 +0800

    package. using static adapter for svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 import makeAttractionsImporter from 'attractions/importer.js';
 
