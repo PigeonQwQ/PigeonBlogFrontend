136ef0b81907113c918893b47ee33971ec0fb5e2
DarcJC <me@darc.pro>
2022-01-10 20:13:06 +0800

    feat. using heti and attraction framework

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,16 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import makeAttractionsImporter from 'attractions/importer.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({
+		scss: {
+			importer: makeAttractionsImporter(),
+		},
+	}),
 
 	kit: {
 		adapter: adapter(),
